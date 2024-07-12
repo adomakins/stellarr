26b9752f3bd47e0cc4ad0f32ad0a854af902221c
adomakins <adamskjervold@gmail.com>
2024-07-12 11:04:30 -0700

    Don't break please

---
+++ app/api/event-router.js
@@ -0,0 +1,32 @@
+// 
+export default function eventRouter(req) {
+
+
+    console.log(`New event received of type: ${req.body.type}`)
+  
+    // Array that holds the different types of events we're expecting and the corresponding handler
+    const handlerPaths = {
+      'OutboundMessage': '../../utils/message-event',
+      'InboundMessage': '../../utils/message-event',
+      'ContactCreate': '../../utils/contact-create',
+      'OpportunityStatusUpdate': '../../utils/opportunity-status'
+    };
+    
+    const handlerPath = handlerPaths[req.body.type];
+    
+    handlerPath ? processEvent(handlerPath, req.body)
+      : console.log(`No handler found for type: ${req.body.type}`);
+
+    res.status(200).json({ text: "Got it bro I'll take it from here" });
+  
+  }
+  
+  // Function to import the module and call the default exported function
+  
+  async function processEvent(handlerPath, reqBody) {
+  
+    console.log(`Calling dynamic function for type: ${reqBody.type}`);
+    const module = await import(handlerPath);
+    module.default(reqBody);
+  
+  }
