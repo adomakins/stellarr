27a6ee896ad4d79f011480c6cde108f716ced83b
adomakins <adamskjervold@gmail.com>
2024-07-12 11:26:25 -0700

    Testinggggggg

---
+++ app/api/hello.js
@@ -0,0 +1,8 @@
+
+export default function helloWorld(req, res) {
+
+    console.log(`New event received of type: ${req.body.type}`)
+
+    res.status(200).json({ text: "Got it bro I'll take it from here" });
+  
+  }
